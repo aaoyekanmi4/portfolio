ce8cde8e8b5c4f01e5e7a8ec89b68fdb71617e1b
Arit Oyekan <aaoyekanmi4@gmail.com>
2019-10-07 14:24:35 -0700

    loading projects by js

---
+++ js/PROJECTS.js
@@ -0,0 +1,13 @@
+const PROJECTS = [{
+    title:"Quiz App",
+    languages:"JavaScript, jQuery",
+    description:`A single-page app about JavaScript array methods. The first Thinkful project. 
+                I learned about changing the DOM dynamically while emphasizing 
+                responsiveness and accessibility.  
+                The app provides the users with links to documentation
+                about questions they missed.`,
+    imageSrc:"images/quiz1.png",
+    imageAlt:"screenshot of quiz app end screen",
+    repo:"https://github.com/aaoyekanmi4/quiz-app",
+    liveSite:"https://aaoyekanmi4.github.io/quiz-app/"
+}];
---
+++ js/showProject.js
@@ -0,0 +1,25 @@
+
+//Generate HTML from PROJECTS.js 
+function showProject (PROJECTS){
+   
+    const htmlArray = PROJECTS.map(project =>
+        `<div class="project-row">
+         <div class="project description">
+         <h2 class="project-title">${project.title}</h2>
+         <h3>Languages/Frameworks: </h3><h3 class="languages">${project.languages}</h3>
+         <p class="description-text">${project.description} </p>
+         <p class="project-links"><a class="project-links" id="repo" target="_blank" href=${project.repo}>Github </a>|
+         <a class="project-links" target="_blank" id ="live" href=${project.livesite}> Live site</a></p>
+         </div>
+         <div class="project project-image">
+         <img alt=${project.imageAlt} src =${project.imageSrc}>
+         </div>
+         </div>`);
+    const projectHTML = htmlArray.join('');
+
+$('.loadProjects').append(projectHTML)
+}
+
+$(showProject(PROJECTS));
+
+console.log(PROJECTS);
