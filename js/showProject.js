4a3a29b8a33a8c072d64a0a2c97b5aa6346da8db
Arit Oyekan <aaoyekanmi4@gmail.com>
2019-10-08 13:29:12 -0700

    fixed typo in variable name

--- js/showProject.js
+++ js/showProject.js
@@ -9,7 +9,7 @@ function showProject (PROJECTS){
          <h3>Languages/Frameworks: </h3><h3 class="languages">${project.languages}</h3>
          <p class="description-text">${project.description} </p>
          <p class="project-links"><a class="project-links" id="repo" target="_blank" href=${project.repo}>Github </a>|
-         <a class="project-links" target="_blank" id ="live" href=${project.livesite}> Live site</a></p>
+         <a class="project-links" target="_blank" id ="live" href=${project.liveSite}> Live site</a></p>
          </div>
          <div class="project project-image">
          <img alt="${project.imageAlt}" src =${project.imageSrc}>
