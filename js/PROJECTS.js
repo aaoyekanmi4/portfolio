71b9320c27c44b12289ed93a33a16a6eada72d6b
Arit Oyekan <aaoyekanmi4@gmail.com>
2019-10-30 12:15:14 -0500

    added API hack to portfolio

--- js/PROJECTS.js
+++ js/PROJECTS.js
@@ -10,4 +10,16 @@ const PROJECTS = [{
     imageAlt:"quiz-app screenshot",
     repo:"https://github.com/aaoyekanmi4/quiz-app",
     liveSite:"https://aaoyekanmi4.github.io/quiz-app"
-}];
+    }, 
+    {
+    title:"Exercise Finder",
+    languages:"JavaScript, jQuery",
+    description:`An app for finding videos and information on various exercises through the YouTube Data API and 
+    workoutmanager (wger) API respectively. Users can search by the name of an exercise, by equipment needed, or main muscle
+    targeted. This project was co-created with Rena Lorkis, a fellow Thinkful student.`,
+    imageSrc:"images/exercise-finder.png",
+    imageAlt:"exercise-finder screenshot",
+    repo:"https://github.com/aaoyekanmi4/Exercise-Finder",
+    liveSite:"https://aaoyekanmi4.github.io/Exercise-Finder"
+    }
+];
