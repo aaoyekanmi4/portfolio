1d3879a7d11010eb476c5b3179f02bccbcbf6e14
Arit Oyekan <aaoyekanmi4@gmail.com>
2019-12-16 15:44:33 -0600

    modified projects section with update description and alternating display

--- js/PROJECTS.js
+++ js/PROJECTS.js
@@ -1,11 +1,10 @@
 const PROJECTS = [{
     title:"Quiz App",
     languages:"JavaScript, jQuery",
-    description:`A single-page app about JavaScript array methods. The first Thinkful project. 
-                I learned about changing the DOM dynamically while emphasizing 
-                responsiveness and accessibility.  
-                The app provides the users with links to documentation
-                about questions they missed.`,
+    description:`A single-page application which quizzes the user on JavaScript array methods.
+                The DOM is updated dynamically using jQuery while emphasizing 
+                responsiveness and accessibility. Users  are linked to helpful 
+                documentation for questions they missed.`,
     imageSrc:"images/quiz2.png",
     imageAlt:"quiz-app screenshot",
     repo:"https://github.com/aaoyekanmi4/quiz-app",
@@ -14,9 +13,9 @@ const PROJECTS = [{
     {
     title:"Exercise Finder",
     languages:"JavaScript, jQuery",
-    description:`An app for finding videos and information on various exercises through the YouTube Data API and 
-    workoutmanager (wger) API respectively. Users can search by the name of an exercise, by equipment needed, or main muscle
-    targeted. This project was co-created with Rena Lorkis, a fellow Thinkful student.`,
+    description:`An app for finding videos and information on different exercises through the YouTube Data API and 
+    workoutmanager (wger) API . Users can search by the name of an exercise, by equipment needed, or main muscle
+    targeted. This project was co-created with Rena Lorkis.`,
     imageSrc:"images/exercise-finder.png",
     imageAlt:"exercise-finder screenshot",
     repo:"https://github.com/aaoyekanmi4/ExerciseFinder",
--- js/showProject.js
+++ js/showProject.js
@@ -2,19 +2,38 @@
 //Generate HTML from PROJECTS.js 
 function showProject (PROJECTS){
    
-    const htmlArray = PROJECTS.map(project =>
-        `<div class="project-row">
-         <div class="project description">
-         <h2 class="project-title">${project.title}</h2>
-         <h3>Languages/Frameworks: </h3><h3 class="languages">${project.languages}</h3>
-         <p class="description-text">${project.description} </p>
-         <p class="project-links"><a class="project-links" id="repo" target="_blank" href=${project.repo}>Github </a>|
-         <a class="project-links" target="_blank" id ="live" href=${project.liveSite}>Live site</a></p>
-         </div>
-         <div class="project project-image">
-         <img alt="${project.imageAlt}" src =${project.imageSrc}>
-         </div>
-         </div>`);
+    const htmlArray = PROJECTS.map(project =>{
+        if (PROJECTS.indexOf(project) % 2 === 0){
+            return `<div class="project-row">
+            <div class="project description">
+            <h2 class="project-title">${project.title}</h2>
+            <h3>Languages/Frameworks: </h3><h3 class="languages">${project.languages}</h3>
+            <p class="description-text">${project.description} </p>
+            <p class="project-links"><a class="project-links" id="repo" target="_blank" href=${project.repo}>Github </a>|
+            <a class="project-links" target="_blank" id ="live" href=${project.liveSite}>Live site</a></p>
+            </div>
+            <div class="project project-image">
+            <a href=${project.liveSite} target="_blank" class="image-link"><img alt="${project.imageAlt}" src =${project.imageSrc}></a>
+            </div>
+            </div>`
+        }
+        else {
+           return `<div class="project-row">
+            <div class="project project-image">
+            <a href=${project.liveSite} target="_blank" class="image-link"><img alt="${project.imageAlt}" src =${project.imageSrc}></a>
+            </div>
+            <div class="project description">
+            <h2 class="project-title">${project.title}</h2>
+            <h3>Languages/Frameworks: </h3><h3 class="languages">${project.languages}</h3>
+            <p class="description-text">${project.description} </p>
+            <p class="project-links"><a class="project-links" id="repo" target="_blank" href=${project.repo}>Github </a>|
+            <a class="project-links" target="_blank" id ="live" href=${project.liveSite}>Live site</a></p>
+            </div>
+           
+            </div>`
+
+        }
+    });
     const projectHTML = htmlArray.join('');
 
 $('.loadProjects').append(projectHTML)
