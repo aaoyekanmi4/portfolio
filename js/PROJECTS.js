48192d6d967c03804dc570c872431f6b19b7c74f
Arit Oyekan <aaoyekanmi4@gmail.com>
2020-02-16 18:00:01 -0600

    changed quiz-app link

--- js/PROJECTS.js
+++ js/PROJECTS.js
@@ -1,34 +1,29 @@
 const PROJECTS = [
-
-    {
-        title:"JavaScript Arrays Quiz",
-        description:`Single page application which tests users knowledge of array methods and provides feedback`,
-        imageSrc:"images/quiz.png",
-        imageAlt:"quiz app screenshot",
-        tech:['jQuery', 'HTML', 'CSS'],
-        repo:"https://github.com/aaoyekanmi4/quiz-app",
-        live:"https://aaoyekanmi4.github.io/quiz-app/"
-    
-        },
-        {
-            title:"Hint",
-            description:`A 2-player clue like game that you can play against a friend. Playable on large screen devices, mobile version in progress.`,
-            imageSrc:"images/hint.png",
-            imageAlt:"item catalog screenshot",
-            tech:['Node', 'Sockets.io', 'jQuery'],
-            repo:"https://github.com/aaoyekanmi4/hint-2player",
-            live:"http://hint-2player.herokuapp.com/"
-        
-            },
-            {
-                title:"Music Catalog",
-                description:`A web application built using Python and Flask which allows users to perform CRUD operations on a catalog of musical instruments.`,
-                imageSrc:"images/itemcatalog1-800.png",
-                imageAlt:"music-catalog screenshot",
-                tech:['Python', 'Flask', 'PostgresSQL'],
-                repo:"https://github.com/aaoyekanmi4/item-catalog",
-                live:"https://music-store-catalog.herokuapp.com/"
-            
-                }
-
+  {
+    title: "JavaScript Arrays Quiz",
+    description: `Single page application which tests users knowledge of array methods and provides documentation for missed answers`,
+    imageSrc: "images/quiz.png",
+    imageAlt: "quiz app screenshot",
+    tech: ["React", "CSS"],
+    repo: "https://github.com/aaoyekanmi4/react-quiz-app",
+    live: " https://react-quiz-app-1g4mvr480.now.sh/"
+  },
+  {
+    title: "Hint",
+    description: `A 2-player clue like game that you can play against a friend. Playable on large screen devices, mobile version in progress.`,
+    imageSrc: "images/hint.png",
+    imageAlt: "item catalog screenshot",
+    tech: ["Node", "Sockets.io", "jQuery"],
+    repo: "https://github.com/aaoyekanmi4/hint-2player",
+    live: "http://hint-2player.herokuapp.com/"
+  },
+  {
+    title: "Music Catalog",
+    description: `A web application built using Python and Flask which allows users to perform CRUD operations on a catalog of musical instruments.`,
+    imageSrc: "images/itemcatalog1-800.png",
+    imageAlt: "music-catalog screenshot",
+    tech: ["Python", "Flask", "PostgresSQL"],
+    repo: "https://github.com/aaoyekanmi4/item-catalog",
+    live: "https://music-store-catalog.herokuapp.com/"
+  }
 ];
